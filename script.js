71b0439ba6083bae13b5e77be78d53e0ca3a0491
Josiel Borges <josielborges555@gmail.com>
2022-06-14 11:55:29 -0300

    entrega finalizada

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Josiel";
+let sobrenome= "Borges";
+let anoNascimento = 2003;
+let cidade = "Curitiba";
+
+let anoAtual = 2022;
+let idade = anoAtual - anoNascimento;
+
+console.log(`Eu sou ${nome} ${sobrenome}, tenho ${idade} anos e moro em ${cidade}.`);
